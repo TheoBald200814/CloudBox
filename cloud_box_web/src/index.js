e081aa16e67f78756c90246a0125e01d8410675d
TheoBald <zhou_rj@icloud.com>
2023-02-16 20:32:20 +0800

    初始化前端框架-导入Material_UI

--- cloud_box_web/src/index.js
+++ cloud_box_web/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Button} from "@mui/material";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <Button variant="contained">Contained</Button>
   </React.StrictMode>
 );
 
