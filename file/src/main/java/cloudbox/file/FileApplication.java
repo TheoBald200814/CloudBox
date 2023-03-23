5f5f0f667bc88984afced137a784022c1c1d38aa
TheoBald <zhou_rj@icloud.com>
2023-03-23 15:24:06 +0800

    文件管理模块：微服务注册配置
    Eureka Client：cloud-box-file-management

--- file/src/main/java/cloudbox/file/FileApplication.java
+++ file/src/main/java/cloudbox/file/FileApplication.java
@@ -3,8 +3,10 @@ package cloudbox.file;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 @MapperScan("cloudbox.file.mapper")
 public class FileApplication {
 
